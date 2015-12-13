904aec08aa8a020978ef7d2f6547a6cda56e4327
takhovski <sh.seddigh@gmail.com>
2015-12-13 14:08:36 -0700

    Plot 2
    The png file for plot 2

---
+++ plot2.R
@@ -0,0 +1,21 @@
+### Set the working directory to where the files are.
+setwd(dir = "c:/Users/Shobeir/Desktop/data/week1/")
+### Read the data and define that "?" is the NA string in this data set
+### Also define that the data are separated via ";" and give the class to the coloumns
+data <- read.table(file = "household_power_consumption.txt",header = TRUE,na.strings = "?",sep = ";",colClasses = c("character","character",rep("numeric",7)))
+### Add a new coloumn's class from character to date.
+data$DateTime <-  strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M") 
+### Convert the Date coloumn's class from character to date.
+data$Date <- as.Date(data$Date,format ="%d/%m/%Y")
+### Subsetting the data and only selecting two days. This reduces the memory usage a lot.
+data <- subset(data,Date=="2007-02-01" | Date=="2007-02-02")
+# add a factor comlumn as what is the day
+data$day <- as.factor(weekdays(data$DateTime,abbreviate=TRUE))
+### Initially used the following line to make sure the data looks correct.
+ str(data)
+### activate the png device and mention the size
+png("plot2.png",width = 480,height = 480)
+### Plotting datails
+plot(y=data$Global_active_power,x=data$DateTime,type = "l",xlab = " ",ylab = "Global Active Power (Kilowatts)")
+### Turning off the png device.
+dev.off()
